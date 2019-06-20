ae935f2a0fdac6ee9ac729018b80d06625bfda83
tryonelove <tryonelove@protonmail.com>
2019-06-21 01:45:00 +0300

    create configs on start

--- botApp.py
+++ botApp.py
@@ -1,11 +1,38 @@
 from bot import Bot
 import json
+import configparser
 
-    
-if __name__ == "__main__":
-    with open("config.json", "r") as f:
-        config = json.load(f)
+def checkConfigs():
+    required = {
+        "config":
+                {   
+                    "token": "",
+                    "osu_api_key": "",
+                    "donators": "",
+                    "weather_api": "",
+                    "prefix": "",
+                    "restricted": [],
+                    "admin": []
+                }, 
+        "maps":{}, 
+        "commands":{}, 
+        "users":{}
+        }
+    for filename in required:
+        try:
+            open(filename+".json")
+        except FileNotFoundError:
+            with open(filename+".json", "w+") as f:
+                json.dump({}, f, indent=4)
+
+def init():
     # osu!community 176954825
     # test 178909901
+    checkConfigs()
+    with open("config.json", "r", encoding="utf-8") as f:
+        config = json.load(f)
     bot = Bot(config["test_token"], 178909901)
     bot.start()
+
+if __name__ == "__main__":
+    init()
